6ac8ea241ee39b743d687c2102aaab7df16a56a3
tural12 <105974985+tural12@users.noreply.github.com>
2022-05-21 17:46:27 +0400

    task2

--- main.cs
+++ main.cs
@@ -1,29 +1,23 @@
 using System;
 
-namespace ConsoleApp
+namespace ConsoleApp10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int Startnumber;
-            int endnumber;
-
-            Console.WriteLine("Bir eded daxil edin");
-            Startnumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Bir eded daxil edin");
-            endnumber = Convert.ToInt32(Console.ReadLine());
-
-            int result = 0;
-
-            for (int i = 0; i <= endnumber; i = i + 1)
-            {
-                result = result + i;
-
-            }
 
 
-            Console.WriteLine(+Startnumber + " bu eded ile " + endnumber + " bu eded arasindaki reqemlerin cemi: " + result.ToString());
+            Console.WriteLine("eded daxil edin");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("eded daxil edin");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("bu ededlerin ededi ortasi: " + (a + b) / 2);
+
+
+
         }
     }
 }
