4a1a4ec1737890737b01551759e325c923c3c85b
tural12 <105974985+tural12@users.noreply.github.com>
2022-05-21 17:40:02 +0400

    task 1
    task 2

---
+++ main.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Startnumber;
+            int endnumber;
+
+            Console.WriteLine("Bir eded daxil edin");
+            Startnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bir eded daxil edin");
+            endnumber = Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+
+            for (int i = 0; i <= endnumber; i = i + 1)
+            {
+                result = result + i;
+
+            }
+
+
+            Console.WriteLine(+Startnumber + " bu eded ile " + endnumber + " bu eded arasindaki reqemlerin cemi: " + result.ToString());
+        }
+    }
+}
